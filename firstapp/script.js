107401f21240387cd9b1344e23cca6b9676cddf5
victormiglio <victormiglio@hotmail.com>
2024-04-22 11:19:13 -0300

    definicao do calculo automatico da idade

--- firstapp/script.js
+++ firstapp/script.js
@@ -1,9 +1,15 @@
+//input
 anoNascimento = 1996
-
-idade = 2024 - anoNascimento
-
 nome = "victor"
 
+//processamento
+hoje = new Date()
+anoAtual = hoje.getFullYear()
+idade = anoAtual - anoNascimento
 mensagem = "Sou " +nome +" e tenho "  +idade+  " anos"
 
+//output
 alert(mensagem)
+
+
+
